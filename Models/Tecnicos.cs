c338fb6e48d815a607d0a2535d92528f2dd983ea
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-08-29 19:31:55 -0400

    Modificacion en Tecnicos

--- Models/Tecnicos.cs
+++ Models/Tecnicos.cs
@@ -8,9 +8,12 @@ namespace RegistroTecnico1.Models
         public int tecnicoId { get; set; }
 
         [Required(ErrorMessage = "Por favor ingrese el nombre del Tecnico")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
         public string nombreTecnico { get; set; }
 
         [Required(ErrorMessage = "Por favor ingrese el sueldo del tecnico")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten números enteros o decimales")]
+
         public float sueldoHora { get; set; }
     }
 }
