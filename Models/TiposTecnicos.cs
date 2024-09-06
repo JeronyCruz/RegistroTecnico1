086809f2882854345571b9b9f68b501f53615fb5
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-09-05 23:04:38 -0400

    Creacion de la clase TiposTecnicos

---
+++ Models/TiposTecnicos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico1.Models;
+
+public class TiposTecnicos
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Por favor ingrese la descripccion del Tecnico")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Descripcion { get; set; }
+}
