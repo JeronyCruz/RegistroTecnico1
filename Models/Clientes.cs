a6e248ee6c31aca5160fc57ca88dfba4e2a1b34f
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-09-10 15:00:15 -0400

    Creacion de la Clase Clientes en Models

---
+++ Models/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico1.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClienteId { get; set; }
+
+    [Required(ErrorMessage ="Por favor ingrese el Nombre del Cliente")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage ="Solo se Permiten Letras")]
+    public string Nombres { get; set; }
+
+    [Required(ErrorMessage ="Por favor, ingrese el numero de WhatsApp")]
+    [RegularExpression(@"^\d+$" , ErrorMessage ="Solo se permiten Numeros")]
+    public string WhatsApp { get; set; }
+}
