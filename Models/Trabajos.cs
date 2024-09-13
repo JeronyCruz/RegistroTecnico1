859d28ea8f5f60d8f4418569f9df5939c132e41d
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-09-13 03:19:22 -0400

    Creacion del Modelo Trabajos

---
+++ Models/Trabajos.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnico1.Models;
+
+public class Trabajos
+{
+    [Key]
+    public int TrabajoId { get; set; }
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "Por favor ingrese la descripccion del Trabajo")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Por favor ingrese el Monto del Trabajo")]
+    [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten números enteros o decimales")]
+    public double Monto { get; set; }
+
+    [ForeignKey("Cliente")]
+    public int ClienteId { get; set; }
+    public Clientes? Cliente { get; set; }
+
+    [ForeignKey("Tecnico")]
+    public int TecnicoId { get; set; }
+    public Tecnicos? Tecnico { get; set; }
+
+
+}
