a9b8df0d20b18792e8a0fb93521792d54a1747ac
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-09-18 14:09:33 -0400

    Creacion de la clase Prioridades

---
+++ Models/Prioridades.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico1.Models;
+
+public class Prioridades
+{
+    [Key]
+    public int PrioridadId { get; set; }
+
+    [Required(ErrorMessage = "Por favor ingrese la Descripcion de la Prioridad")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Descripcion { get; set; }
+
+    [Required(ErrorMessage ="Por favor ingrese el Tiempo de la Prioridad")]
+    [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten números enteros o decimales")]
+
+    public int Tiempo { get; set; }
+}
