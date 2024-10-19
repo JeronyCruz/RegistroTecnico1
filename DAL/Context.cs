116909ac1d460a28ca3ec1ceaf7b4a6dcafc98f4
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-10-19 13:05:33 -0400

    Modificacion en Context

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -12,5 +12,19 @@ namespace RegistroTecnico1.DAL;
         public DbSet<Clientes> Clientes { get; set; }
         public DbSet<Trabajos> Trabajos { get; set; }
         public DbSet<Prioridades> Prioridades { get; set; }
+        public DbSet<Articulos> Articulos { get; set; }
+        public DbSet<TrabajosDetalle> TrabajosDetalles { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Articulos>().HasData(new List<Articulos>()
+        {
+            new Articulos(){ArticuloId = 1, Descripcion = "Memoria USB", Costo = 370, Precio = 450, Existencia = 30},
+			new Articulos(){ArticuloId = 2, Descripcion = "JoyStick", Costo = 890, Precio = 1200, Existencia = 70},
+			new Articulos(){ArticuloId = 3, Descripcion = "Cable UTP", Costo = 250, Precio = 410, Existencia = 60},
+			new Articulos(){ArticuloId = 4, Descripcion = "Pasta Termica", Costo = 610, Precio = 730, Existencia = 50}
+		});
+	}
 }
 
