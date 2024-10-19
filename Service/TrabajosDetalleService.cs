1c3b6b0259b202f7fe60e75faa7b7748c0509f9e
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-10-19 13:01:55 -0400

    Creacion de Trabajos Detalle Service

---
+++ Service/TrabajosDetalleService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico1.DAL;
+using RegistroTecnico1.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnico1.Service;
+
+public class TrabajosDetalleService
+{
+	private readonly Context _context;
+
+	public TrabajosDetalleService(Context context)
+	{
+		_context = context;
+	}
+
+    public async Task<List<Articulos>> Listar(Expression<Func<Articulos, bool>> criterio)
+    {
+        return await _context.Articulos
+			.AsNoTracking()
+			.Where(criterio)
+			.ToListAsync();
+    }
+}
+   
